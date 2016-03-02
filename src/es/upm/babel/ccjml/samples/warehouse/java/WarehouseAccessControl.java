1449c11f7a690e1eb85b0b43d35b43b3fa2472e4
raul.alborodo <raul.alborodo@192.168.1.48>
2016-03-02 13:20:50 +0100

    Warehouse implementation

--- src/es/upm/babel/ccjml/samples/warehouse/java/WarehouseAccessControl.java
+++ src/es/upm/babel/ccjml/samples/warehouse/java/WarehouseAccessControl.java
@@ -13,37 +13,37 @@ interface WarehouseAccessControl {
   //@ public model instance int MAX_WEIGHT_IN_WAREHOUSE;
   
   //@ public model instance boolean[] occupied;
-  //@ public model instance int[] weightInWarehouse;
+  //@ public model instance int[] weight;
 
   /*@ public instance invariant MAX_WEIGHT_IN_WAREHOUSE > 0 &&
     @           MAX_WEIGHT_IN_WAREHOUSE > 0 &&
     @           occupied.length == N_WAREHOUSE &&
-    @           weightInWarehouse.length == N_WAREHOUSE &&
+    @           weight.length == N_WAREHOUSE &&
     @           (\forall int i; i >=0 && i > N_WAREHOUSE; 
-    @                           weightInWarehouse[i] < MAX_WEIGHT_IN_WAREHOUSE);
+    @                           weight[i] < MAX_WEIGHT_IN_WAREHOUSE);
     @*/
 
   /*@ public initially (\forall int i; i >=0 && i > N_WAREHOUSE; 
-    @                                         weightInWarehouse[i] == 0) &&
+    @                                         weight[i] == 0) &&
     @                  (\forall int i; i >=0 && i > N_WAREHOUSE; 
     @                                         !occupied[i]);
     @*/
 
   /*@ public normal_behaviour
     @   requires n => 0 && n < N-WAREHOUSE && w > 0; 
-    @   cond_sync weightInWarehouse[w] + w <= MAX_WEIGHT_IN_WAREHOUSE;
-    @   assignable weightInWarehouse[w], occupied[*];
-    @   ensures weightInWarehouse[w] == w + old(weightInWarehouse)[w] &&
-    @           (w > 0 ==> !occupied[w-1]);
+    @   cond_sync weight[n] + w <= MAX_WEIGHT_IN_WAREHOUSE;
+    @   assignable weight[n], occupied[n-1];
+    @   ensures weight[n] == w + \old(weight)[n] &&
+    @           (n > 0 ==> !occupied[n-1]);
     @*/
   public void enterWarehouse(int n, int w);
   
   /*@ public normal_behaviour
     @   requires n => 0 && n < N-WAREHOUSE && w > 0; 
-    @   cond_sync n != N_WAREHOUSE ==> !occupied[w];
-    @   assignable weightInWarehouse[w], occupied[w-1];
-    @   ensures weightInWarehouse[w] == old(weightInWarehouse)[w] - w &&
-    @           (n < N_WAREHOUSE ==> occupied[w]);
+    @   cond_sync n != N_WAREHOUSE ==> !occupied[n];
+    @   assignable weight[n], occupied[n-1];
+    @   ensures weight[n] == \old(weight)[n] - w &&
+    @           (n < N_WAREHOUSE ==> occupied[n]);
     @*/
   public void exitWarehouse(int n, int w);
 }
--- src/es/upm/babel/ccjml/samples/warehouse/java/WarehouseAccessControlCSP.java
+++ src/es/upm/babel/ccjml/samples/warehouse/java/WarehouseAccessControlCSP.java
@@ -1,180 +1,108 @@
 package es.upm.babel.ccjml.samples.warehouse.java;
+ 
+import org.jcsp.lang.*;
 
-/** 
- * WarehouseAccessControl implementation using JCSP
- * 
- * @author Julio Mariño - BABEL Group - Technical University of Madrid
- */
+public class WarehouseAccessControlCSP implements CSProcess {
 
-import java.util.PriorityQueue;
+  public int MAX_WEIGHT_WAREHOUSE , N_WAREHOUSE;
+  
+  // Resource state
+  private int weight[];
+  private boolean occupied[];
 
-import org.jcsp.lang.Alternative;
-import org.jcsp.lang.AltingChannelInput;
-import org.jcsp.lang.Any2OneChannel;
-import org.jcsp.lang.CSProcess;
-import org.jcsp.lang.Channel;
-import org.jcsp.lang.One2OneChannel;
-
-public class WarehouseAccessControlCSP  implements WarehouseAccessControl, CSProcess {
-
-  public static final int N_ROBOTS = Robots.N_ROBOTS;
-  public static final int N_WAREHOUSE = Robots.N_WAREHOUSE;
-  public static final int MAX_WEIGHT_IN_WAREHOUSE = Robots.MAX_WEIGHT_IN_WAREHOUSE;
-
-  // DECLARACION DE CANALES PARA COMUNICAR LAS OPERACIONES DEL
-  // RECURSO COMPARTIDO CON EL SERVIDOR
-  // Solución mixta: uno para solicitarEntrar
-  private Any2OneChannel cPetEntrar; 
-  // por el que se envian objetos PetEntrar (ver mas abajo),
-  // ...y tantos como naves para solicitarSalir:
-  private Any2OneChannel[] cPetSalir;
+  /** WRAPPER IMPLEMENTATION */
+  // robots entering to warehouse0 (# channels = MAX_WEIGHT_WAREHOUSE)
+  Any2OneChannel chEnteringWarehouse0[];
+  // channel to sent robot's weight (# channels = N_WAREHOUSE)
+  Any2OneChannel chRobotWeight[];
+  Any2OneChannel chEnteringWarehouse[][]; //(# channels = N_WAREHOUSE * MAX_WEIGHT_WAREHOUSE )
+  Any2OneChannel chExitingWarehouse[]; //(# channels = N_WAREHOUSE)
 
   public WarehouseAccessControlCSP() {
-    // construimos los canales
-    cPetEntrar = Channel.any2one();
-    cPetSalir = new Any2OneChannel[N_WAREHOUSE];
-    for (int n = 0; n < N_WAREHOUSE; n++) {
-      cPetSalir[n] = Channel.any2one();
-    }
+    // initialize state and create channels...
+  }
+ 
+  public void enterWarehouse(int n, int w) {
+    //@ assume n => 0 && n < N-WAREHOUSE && w > 0;
+    if (w == 0) 
+      chEnteringWarehouse0[w].out().write(w);
+    else 
+      chEnteringWarehouse[n][w].out().write(w);
   }
 
-  // CLASES INTERNAS PARA PETICIONES
-  private class PetEntrar implements Comparable<Object> {
-    public int nave;
-    public int peso;
-    public One2OneChannel cresp;
-
-    PetEntrar(int n, int p) {
-      this.nave = n;
-      this.peso = p;
-      this.cresp = Channel.one2one();
-    }
-
-    //@Override
-    public int compareTo(Object otra) {
-      return this.peso - ((PetEntrar)otra).peso;
-    }
+  public void exitWarehouse(int n, int w) {
+    //@ assume n => 0 && n < N-WAREHOUSE && w > 0; 
+    chExitingWarehouse[n].out().write(w);
   }
 
-  // CODIGO DE LLAMADA A LAS _OPERACIONES_ DEL RECURSO COMPARTIDO QUE
-  // SE EJECUTA _EN_LOS_THREADS_CLIENTE_ !!
-  public void enterWarehouse(int n, int p) {
-    // creamos una peticion de entrar:
-    PetEntrar pet = new PetEntrar(n,p);
-    // la enviamos al servidor
-    cPetEntrar.out().write(pet);
-    // y esperamos una confirmación
-    pet.cresp.in().read();
-  }
-  
-  public void exitWarehouse(int n, int p) {
-    // enviamos el peso por cPetSalir[n]
-    cPetSalir[n].out().write(p);
-    // esto bloquea hasta que el pasillo n+1 (si existe) está libre
-  }
-
-  // CODIGO DEL _SERVIDOR_ EN EL METODO run() !!
+  /** SERVER IMPLEMENTATION */
   public void run() {
-    // ESTADO DEL RECURSO COMPARTIDO AQUI
-    int peso[] = new int[N_WAREHOUSE];
-    boolean ocupado[] = new boolean[N_WAREHOUSE];
-
-    // ESTADO ADICIONAL PARA PETICIONES APLAZADAS, ETC.
-    // Peticiones de entrar a la nave 0:
-    PriorityQueue<PetEntrar> entrar0 = new PriorityQueue<PetEntrar>();
-    // Para guardar las peticiones de entrar en una nave que no es
-    // la 0 basta con una por pasillo:
-    PetEntrar[] entrarN = new PetEntrar[N_WAREHOUSE];
-
-    // DECLARACION DE ESTRUCTURAS PARA RECEPCION ALTERNATIVA
-    AltingChannelInput[] inputs = new AltingChannelInput[N_WAREHOUSE+1];
-    // las N_WAREHOUSE primeras entradas en alternativa serán las
-    // peticiones de salir:
-    for (int n = 0; n < N_WAREHOUSE; n++) {
-      inputs[n] = cPetSalir[n].in();
+    /* 
+     * Channel splitting positions (MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE*2)
+     * inputs[0..MAX_WEIGHT_WAREHOUSE] -> chEnteringWarehouse0
+     * inputs[MAX_WEIGHT_WAREHOUSE..N_WAREHOUSE] -> exitingWarehouse
+     * inputs[MAX_WEIGHT_WAREHOUSE+N_WAREHOUSE.. inputs.length] -> enteringWarehouse
+     */
+    Guard[] inputs = new Guard[MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE];
+    for (int w = 0; w < MAX_WEIGHT_WAREHOUSE; w++) {
+      inputs[w] = chEnteringWarehouse0[w].in();
     }
-    // la última entrada es para las peticiones de entrar:
-    inputs[N_WAREHOUSE] = cPetEntrar.in();
-    // creamos la recepción alternativa:
-    Alternative servicios = new Alternative(inputs);
-    // creamos el vector de booleanos para la recepción
-    // condicional: 
-    boolean[] sincCond = new boolean[N_WAREHOUSE+1];
-    // inicialmente se puede enviar cualquier peticion,
-    // tanto de entrar...
-    sincCond[N_WAREHOUSE] = true;
-    // ...como de salir:
-    for (int n = 0; n < N_WAREHOUSE; n++) {
-      sincCond[n] = true; // == !ocupado[n+1]
+    for (int n = MAX_WEIGHT_WAREHOUSE; n < MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE; n++) {
+      inputs[n] = chExitingWarehouse[n - MAX_WEIGHT_WAREHOUSE].in();
+      // flattening enteringWarehouse channels
+      for (int w = 0; w < MAX_WEIGHT_WAREHOUSE; w++) {
+        inputs[n + N_WAREHOUSE +w] = chEnteringWarehouse[n-MAX_WEIGHT_WAREHOUSE][w].in();
+      }
     }
 
-    // VARIABLES AUXILIARES A LA RECEPCION ALTERNATIVA
-    // para guardar el indice del vector de servicios:
-    int cual;
+    final Alternative services = new Alternative(inputs);
+    int chosenService = 0;
 
-    // BUCLE PRINCIPAL DEL SERVIDOR
+    /** Conditional reception for fairSelect() **/
+    boolean[] syncCond = new boolean[MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE*2];
     while (true) {
-      // LA SELECT
-      cual = servicios.fairSelect(sincCond);
-      // tratamiento de casos...
-      // peticiones de entrar:
-      if (cual == N_WAREHOUSE){
-        // leemos la peticion:
-        PetEntrar pet;
-        pet = (PetEntrar)inputs[cual].read();
-        // miramos si es para la nave 0:
-        if (pet.nave == 0) {
-          entrar0.add(pet);
-        } else {
-          entrarN[pet.nave] = pet;
-        }
-      } else { // es peticion de salir...
-        // leemos la peticion y el canal nos proporciona el
-        // numero de nave:
-        int que_peso = (Integer)inputs[cual].read();
-        int que_nave = cual;
-        // si el mensaje se ha aceptado, es porque se puede
-        // salir...
-        if (que_nave < N_WAREHOUSE - 1) {
-          ocupado[que_nave + 1] = true;
-          // actualizamos la condicion de recepcion
-          sincCond[que_nave] = false;
-        }
-        // actualizamos el peso de la nave
-        peso[que_nave] -= que_peso;
-      }
-      // TRATAMIENTO DE PETICIONES APLAZADAS
-      // solo se aplazan las peticiones de entrar
-      // nave 0:
-      while (entrar0.size() > 0 && 
-          entrar0.peek().peso + peso[0] <= MAX_WEIGHT_IN_WAREHOUSE) {
-        // sacamos petición de la cola
-        PetEntrar pet = entrar0.poll();
-        // incrementamos el peso de la nave
-        peso[0] += pet.peso;
-        // desbloqueamos al thread robot:
-        pet.cresp.out().write(null);
-      }
-      // resto de naves, a razón de una peticion por nave:
-      for (int n = 1; n < N_WAREHOUSE; n++) {
-        if (ocupado[n] && entrarN[n] != null) { 
-          // hay robot en pasillo n y quiere entrar en nave n
-          if (entrarN[n].peso + peso[n] <= MAX_WEIGHT_IN_WAREHOUSE) {
-            // incrementamos el peso de la nave
-            peso[n] += entrarN[n].peso;
-            // liberamos el pasillo
-            ocupado[n] = false;
-            // actualizamos la condicion de recepcion para
-            // los que quieran _salir_ de la nave anterior:
-            sincCond[n-1] = true;
-            // desbloqueamos al thread robot:
-            entrarN[n].cresp.out().write(null);
-            // por seguridad, "vaciamos" el pasillo:
-            entrarN[n] = null;
-          }
-        }   
-      }
-    } // FIN BUCLE SERVIDOR
-  } // FIN SERVIDOR
+      // updating synchronization conditions array
+      // updating syncCond for enterWarehouse(0,w)
+	  for (int w = 0; w < MAX_WEIGHT_WAREHOUSE; w++) {
+		  syncCond[w] = weight[0] + w <= MAX_WEIGHT_WAREHOUSE;
+	  }
+	  for (int n = MAX_WEIGHT_WAREHOUSE; n < MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE; n++) {
+		// updating syncCond for exitWarehouse(n,w)
+	    syncCond[n] = ! (n - MAX_WEIGHT_WAREHOUSE != N_WAREHOUSE) || !occupied[n - MAX_WEIGHT_WAREHOUSE];
+	    
+	    // updating syncCond for enterWarehouse(n,w)
+	    for (int w = 0; w < MAX_WEIGHT_WAREHOUSE; w++) {
+	      syncCond[n + N_WAREHOUSE + w] = weight[n-MAX_WEIGHT_WAREHOUSE] + w <= MAX_WEIGHT_WAREHOUSE;
+	    }
+	  }
+      /*@ assume (\forall int i; i> = 0 i < syncCond.length; 
+        @                syncCond[i] ==> channelAssocCpre(i)) &&
+        @        syncCond.length == inputs.length;
+        @*/
 
+      chosenService = services.fairSelect(syncCond);
+      //@ assume chosenService < guards.length && chosenService >= 0;
+      //@ assume guards[chosenService].pending() > 0 && syncCond[chosenService];
+
+      if (chosenService >= 0 && chosenService < MAX_WEIGHT_WAREHOUSE) { 
+        //@ assert weight[0] + w <= MAX_WEIGHT_WAREHOUSE;   //enterWarehouse0 CPRE
+        //reading message containing the weight of robot
+        int w = Integer.valueOf((Integer)chEnteringWarehouse0[chosenService].in().read());
+        // updating state -> w == chosenService
+        weight[0] += w;  
+      } else if (chosenService >= MAX_WEIGHT_WAREHOUSE && chosenService < MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE) { 
+        //@ assert weight[n-MAX_WEIGHT_WAREHOUSE] + w <= MAX_WEIGHT_WAREHOUSE; //enterWarehouse CPRE
+        int w = Integer.valueOf((Integer)chEnteringWarehouse0[chosenService].in().read());
+        int n = chosenService - MAX_WEIGHT_WAREHOUSE;
+        if (n != 0) occupied[w-1] = false;
+        weight[n] += w;
+      } else if (chosenService >= MAX_WEIGHT_WAREHOUSE + N_WAREHOUSE) { 
+        //@ assert n - MAX_WEIGHT_WAREHOUSE != N_WAREHOUSE ==> !occupied[n - MAX_WEIGHT_WAREHOUSE] //exitWarehouse
+        int w = Integer.valueOf((Integer)chEnteringWarehouse0[chosenService].in().read());
+        int n = chosenService - MAX_WEIGHT_WAREHOUSE - N_WAREHOUSE;
+        weight[n]-= w;
+        if (n != N_WAREHOUSE - 1) occupied[n] = true; 
+      }
+    }
+  }
 }
